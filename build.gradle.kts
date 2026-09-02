c7beefcbcec2fc71d7c294bf99ac18c28e35a263
Boy Offi9 <boyxoffi9@gmail.com>
2026-09-02 03:06:09 -0700

    fix: add JitPack repository to build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,3 +2,11 @@ plugins {
     id("com.android.application") version "8.5.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 }
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") }
+    }
+}
